ada2ef7ac406c3afc9c63a44944ec9719a28def9
Mella <melladjs@hotmail.com>
2022-03-13 14:31:53 +0100

    vista nombres e imagen
    creacion de profile

--- ionic-ricky-morty/src/app/userlist/userlist.page.ts
+++ ionic-ricky-morty/src/app/userlist/userlist.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-userlist',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserlistPage implements OnInit {
 
-  constructor() { }
+  characters = []
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.http.get<any>('https://rickandmortyapi.com/api/character')
+      .subscribe(res => {
+        console.log(res);
+        this.characters = res.results;
+      })
   }
 
 }
